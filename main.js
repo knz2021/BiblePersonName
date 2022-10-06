e2d1da92cceb03099b457bf5c1eef0beed406889
KNZ <86813245+knz2021@users.noreply.github.com>
2022-10-06 21:51:45 +0900

    Fst

---
+++ main.js
@@ -0,0 +1,192 @@
+function clickDisplay1() {
+  const element = document.getElementById("text-button1");
+  element.innerHTML = "ダビデ";
+}
+function clickDisplay2() {
+  const element = document.getElementById("text-button2");
+  element.innerHTML = "アブラハム";
+}
+function clickDisplay3() {
+  const element = document.getElementById("text-button3");
+  element.innerHTML = "サムソン";
+}
+function clickDisplay4() {
+  const element = document.getElementById("text-button4");
+  element.innerHTML = "サムエル";
+}
+function clickDisplay5() {
+  const element = document.getElementById("text-button5");
+  element.innerHTML = "リベカ";
+}
+function clickDisplay6() {
+  const element = document.getElementById("text-button6");
+  element.innerHTML = "モーセ";
+}
+function clickDisplay7() {
+  const element = document.getElementById("text-button7");
+  element.innerHTML = "ペテロ";
+}
+function clickDisplay8() {
+  const element = document.getElementById("text-button8");
+  element.innerHTML = "イサク";
+}
+function clickDisplay9() {
+  const element = document.getElementById("text-button9");
+  element.innerHTML = "ラハブ";
+}
+function clickDisplay10() {
+  const element = document.getElementById("text-button10");
+  element.innerHTML = "イエス";
+}
+function clickDisplay11() {
+  const element = document.getElementById("text-button11");
+  element.innerHTML = "ギデオン";
+}
+function clickDisplay12() {
+  const element = document.getElementById("text-button12");
+  element.innerHTML = "バルナバ";
+}
+function clickDisplay13() {
+  const element = document.getElementById("text-button13");
+  element.innerHTML = "ダニエル";
+}
+function clickDisplay14() {
+  const element = document.getElementById("text-button14");
+  element.innerHTML = "ヨナ";
+}
+function clickDisplay15() {
+  const element = document.getElementById("text-button15");
+  element.innerHTML = "マタイ";
+}
+function clickDisplay16() {
+  const element = document.getElementById("text-button16");
+  element.innerHTML = "ドルカス";
+}
+function clickDisplay17() {
+  const element = document.getElementById("text-button17");
+  element.innerHTML = "ハバクク";
+}
+function clickDisplay18() {
+  const element = document.getElementById("text-button18");
+  element.innerHTML = "マリア、ミリアム";
+}
+function clickDisplay19() {
+  const element = document.getElementById("text-button19");
+  element.innerHTML = "ヒゼキヤ";
+}
+function clickDisplay20() {
+  const element = document.getElementById("text-button20");
+  element.innerHTML = "ナオミ";
+}
+function clickDisplay21() {
+  const element = document.getElementById("text-button21");
+  element.innerHTML = "ヨシヤ";
+}
+function clickDisplay22() {
+  const element = document.getElementById("text-button22");
+  element.innerHTML = "ハンナ";
+}
+function clickDisplay23() {
+  const element = document.getElementById("text-button23");
+  element.innerHTML = "ヨブ";
+}
+function clickDisplay24() {
+  const element = document.getElementById("text-button24");
+  element.innerHTML = "アモス";
+}
+function clickDisplay25() {
+  const element = document.getElementById("text-button25");
+  element.innerHTML = "エリシャ";
+}
+function clickDisplay26() {
+  const element = document.getElementById("text-button26");
+  element.innerHTML = "ソロモン";
+}
+function clickDisplay27() {
+  const element = document.getElementById("text-button27");
+  element.innerHTML = "エズラ";
+}
+function clickDisplay28() {
+  const element = document.getElementById("text-button28");
+  element.innerHTML = "サラ";
+}
+function clickDisplay29() {
+  const element = document.getElementById("text-button29");
+  element.innerHTML = "ネヘミヤ";
+}
+function clickDisplay30() {
+  const element = document.getElementById("text-button30");
+  element.innerHTML = "トマス";
+}
+function clickDisplay31() {
+  const element = document.getElementById("text-button31");
+  element.innerHTML = "エホバ";
+}
+function clickDisplay32() {
+  const element = document.getElementById("text-button32");
+  element.innerHTML = "ヤコブ";
+}
+function clickDisplay33() {
+  const element = document.getElementById("text-button33");
+  element.innerHTML = "エリヤ";
+}
+function clickDisplay34() {
+  const element = document.getElementById("text-button34");
+  element.innerHTML = "エノク";
+}
+function clickDisplay35() {
+  const element = document.getElementById("text-button35");
+  element.innerHTML = "ラケル";
+}
+function clickDisplay36() {
+  const element = document.getElementById("text-button36");
+  element.innerHTML = "イザヤ";
+}
+function clickDisplay37() {
+  const element = document.getElementById("text-button37");
+  element.innerHTML = "ノア";
+}
+function clickDisplay38() {
+  const element = document.getElementById("text-button38");
+  element.innerHTML = "ナタナエル";
+}
+function clickDisplay39() {
+  const element = document.getElementById("text-button39");
+  element.innerHTML = "アビガイル";
+}
+function clickDisplay40() {
+  const element = document.getElementById("text-button40");
+  element.innerHTML = "ステファノ";
+}
+function clickDisplay41() {
+  const element = document.getElementById("text-button41");
+  element.innerHTML = "";
+}
+function clickDisplay42() {
+  const element = document.getElementById("text-button42");
+  element.innerHTML = "";
+}
+function clickDisplay43() {
+  const element = document.getElementById("text-button43");
+  element.innerHTML = "";
+}
+function clickDisplay44() {
+  const element = document.getElementById("text-button44");
+  element.innerHTML = "";
+}
+function clickDisplay45() {
+  const element = document.getElementById("text-button45");
+  element.innerHTML = "";
+}
+function clickDisplay46() {
+  const element = document.getElementById("text-button46");
+  element.innerHTML = "";
+}
+function clickDisplay47() {
+  const element = document.getElementById("text-button47");
+  element.innerHTML = "";
+}
+function clickDisplay48() {
+  const element = document.getElementById("text-button48");
+  element.innerHTML = "";
+}
